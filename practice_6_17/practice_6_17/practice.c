9bfe06b89b87787930f5f8370200eb2271b9371a
yyh888 <1161752748@qq.com>
2022-06-17 22:17:23 +0800

    力扣

---
+++ practice_6_17/practice_6_17/practice.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+//    int* tmp = (int*)malloc(sizeof(int) * 2);
+//    *returnSize = 2;
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//        for (int j = i; j < numsSize; j++)
+//        {
+//            if (nums[i] + nums[j] == target && i != j)
+//            {
+//                tmp[0] = i;
+//                tmp[1] = j;
+//                return tmp;
+//            }
+//        }
+//    }
+//    return NULL;
+//}
+
+
+
