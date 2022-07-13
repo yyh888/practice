17636f1c639ea701b7e55ed63f8644d4643f5343
yyh888 <1161752748@qq.com>
2022-07-13 15:49:04 +0800

    Day02

---
+++ Day02/Day02/practice.c
@@ -0,0 +1,31 @@
+//int main()
+//{
+//    int m = 0;
+//    scanf("%d", &m);
+//    int mid = m * m;
+//    int start = mid - m + 1;
+//    printf("%d", start);
+//    for (int i = 1; i < m; i++)
+//    {
+//        printf("+%d", start + i * 2);
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int gap = 3;
+//    int sum = 2;
+//    int ret = 2;
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        ret += gap;
+//        sum += ret;
+//    }
+//    printf("%d", sum);
+//    return 0;
+//}
