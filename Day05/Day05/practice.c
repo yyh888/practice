f42b209238c49384527bdb168221d5637ed3309a
yyh888 <1161752748@qq.com>
2022-07-19 21:55:31 +0800

    Day05

---
+++ Day05/Day05/practice.c
@@ -0,0 +1,19 @@
+//int convertInteger(int A, int B) {
+//    int ret = A ^ B;
+//    //统计1的个数
+//    int count = 0;
+//    int n = 32;
+//    while (n--)
+//    {
+//        int tmp = ret & 1;
+//        if (tmp == 1)
+//        {
+//            count++;
+//        }
+//        ret >>= 1;
+//    }
+//    return count;
+//}
+
+
+
