14de0c9af1d6611ef2b1b9a78a921b3ad05607d1
yyh888 <1161752748@qq.com>
2022-08-06 09:07:42 +0800

    practice_8_6

---
+++ practice_8_5/practice_8_5/practice.c
@@ -0,0 +1,127 @@
+//typedef int STDataType;
+//typedef struct Stack
+//{
+//    STDataType* _a;
+//    int _top;		// 栈顶
+//    int _capacity;  // 容量 
+//}Stack;
+//// 初始化栈 
+//void StackInit(Stack* ps);
+//// 入栈 
+//void StackPush(Stack* ps, STDataType data);
+//// 出栈 
+//void StackPop(Stack* ps);
+//// 获取栈顶元素 
+//STDataType StackTop(Stack* ps);
+//// 获取栈中有效元素个数 
+//int StackSize(Stack* ps);
+//// 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
+//bool StackEmpty(Stack* ps);
+//// 销毁栈 
+//void StackDestroy(Stack* ps);
+//// 初始化栈 
+//void StackInit(Stack* ps)
+//{
+//    assert(ps);
+//    ps->_a = NULL;
+//    ps->_capacity = ps->_top = 0;
+//}
+//// 入栈 
+//void StackPush(Stack* ps, STDataType data)
+//{
+//    assert(ps);
+//    //满了就扩容
+//    if (ps->_capacity == ps->_top)
+//    {
+//        int newcapacity = ps->_capacity == 0 ? 4 : ps->_capacity * 2;
+//        STDataType* tmp = (STDataType*)realloc(ps->_a, newcapacity * sizeof(STDataType));
+//        if (tmp == NULL)
+//        {
+//            printf("realloc fail");
+//            exit(-1);
+//        }
+//        ps->_a = tmp;
+//        ps->_capacity = newcapacity;
+//    }
+//    ps->_a[ps->_top] = data;
+//    ps->_top++;
+//}
+//// 出栈 
+//void StackPop(Stack* ps)
+//{
+//    assert(ps);
+//    assert(!StackEmpty(ps));
+//    ps->_top--;
+//}
+//// 获取栈顶元素 
+//STDataType StackTop(Stack* ps)
+//{
+//    assert(ps);
+//    assert(!StackEmpty(ps));
+//    return ps->_a[ps->_top - 1];
+//}
+//// 获取栈中有效元素个数 
+//int StackSize(Stack* ps)
+//{
+//    assert(ps);
+//    return ps->_top;
+//}
+//// 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
+//bool StackEmpty(Stack* ps)
+//{
+//    return ps->_top == 0;
+//}
+//// 销毁栈 
+//void StackDestroy(Stack* ps)
+//{
+//    assert(ps);
+//    free(ps->_a);
+//    ps->_capacity = ps->_top = 0;
+//    ps->_a = NULL;
+//}
+//
+//bool isValid(char* s) {
+//    if (s == NULL)
+//    {
+//        return true;
+//    }
+//    Stack st;
+//    StackInit(&st);
+//    while (*s)
+//    {
+//        if (*s == '(' || *s == '[' || *s == '{')
+//        {
+//            StackPush(&st, *s);
+//        }
+//        else
+//        {
+//            if (StackEmpty(&st))
+//            {
+//                return false;
+//            }
+//            else
+//            {
+//                char tmp = StackTop(&st);
+//                StackPop(&st);
+//                if ((tmp == '(' && *s != ')') ||
+//                    (tmp == '{' && *s != '}') ||
+//                    (tmp == '[' && *s != ']'))
+//                {
+//                    StackDestroy(&st);
+//                    return false;
+//                }
+//            }
+//        }
+//        s++;
+//    }
+//    if (!StackEmpty(&st))
+//    {
+//        StackDestroy(&st);
+//        return false;
+//    }
+//    StackDestroy(&st);
+//    return true;
+//}
+
+
+
