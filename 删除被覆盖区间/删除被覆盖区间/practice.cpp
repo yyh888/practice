b67fb95d562db5b408eee94c4bcbf459a4b85358
yyh888 <1161752748@qq.com>
2023-01-23 20:26:51 +0800

    删除被覆盖区间

---
+++ 删除被覆盖区间/删除被覆盖区间/practice.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        for (int i = 0; i < intervals.size(); i++)
+        {
+            for (int j = 0; j < intervals.size(); j++)
+            {
+                if (i != j && intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1])
+                {
+                    n--;
+                    break;
+                }
+            }
+        }
+        return n;
+    }
+};
