2b47dadef53825272a88ee18e8735fa7491941ce
yyh888 <1161752748@qq.com>
2023-01-28 20:15:07 +0800

    蓝桥杯训练6

---
+++ 蓝桥杯训练6/蓝桥杯训练6/practice.cpp
@@ -0,0 +1,22 @@
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    string s;
+//    cin >> n >> s;
+//    for (int i = 0; i < s.size(); i++)
+//    {
+//        if (s[i] >= 'A' && s[i] <= 'Z')
+//        {
+//            s[i] += 32;
+//        }
+//    }
+//    sort(s.begin(), s.end());
+//    cout << s << endl;
+//    return 0;
+//}
+
+
