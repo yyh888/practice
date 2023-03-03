516d315f6238872349f84ad513b55a5dd27c650d
yyh888 <1161752748@qq.com>
2023-03-03 21:16:55 +0800

    从1到N打印

---
+++ practice/剑指Offerday8/剑指Offerday8/practice.cpp
@@ -0,0 +1,11 @@
+// 打印从1到最大的n位数
+
+//class Solution {
+//public:
+//    vector<int> printNumbers(int n) {
+//        int N = pow(10, n);
+//        vector<int> v;
+//        for (int i = 1; i < N; i++) v.push_back(i);
+//        return v;
+//    }
+//};
