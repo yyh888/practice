4cefab71db1fd6eb00804c6d1ca22bbb9cb7746e
yyh888 <1161752748@qq.com>
2023-03-15 13:33:08 +0800

    笔试训练15

---
+++ 笔试训练15/笔试训练15/practice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+//
+//int lowbit(int x)
+//{
+//    return x & -x;
+//}
+//
+//int main()
+//{
+//    int n;
+//    while (cin >> n)
+//    {
+//        int res = 0;
+//        while (n)
+//        {
+//            res++;
+//            n -= lowbit(n);
+//        }
+//        cout << res << endl;
+//    }
+//    return 0;
+//}
