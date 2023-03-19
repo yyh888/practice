eec3f9d56a8eb10cde32e44fcf4c6e0f2419a511
yyh888 <1161752748@qq.com>
2023-03-19 11:55:40 +0800

    每个月兔子数

---
+++ 笔试训练day18/笔试训练day18/practice.cpp
@@ -0,0 +1,21 @@
+// 统计每个月的兔子数
+//#include <iostream>
+//
+//using namespace std;
+//
+//const int N = 32;
+//int a[N];
+//
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    a[1] = 1, a[2] = 1;
+//    for (int i = 3; i <= n; i++)
+//    {
+//        a[i] = a[i - 1] + a[i - 2];
+//    }
+//    cout << a[n];
+//    return 0;
+//}
+
