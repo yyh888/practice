caa312e63da1b9d16b76dc896cf658e2675a074c
yyh888 <1161752748@qq.com>
2023-03-24 21:03:20 +0800

    微信红包

---
+++ 笔试训练23/笔试训练23/practice.cpp
@@ -0,0 +1,21 @@
+// Î¢ÐÅºì°ü
+//class Gift {
+//public:
+//    int getValue(vector<int> gifts, int n) {
+//        unordered_map<int, int> hash;
+//        for (int& e : gifts)
+//        {
+//            hash[e]++;
+//        }
+//        for (auto& e : hash)
+//        {
+//            if (e.second > n / 2)
+//            {
+//                return e.first;
+//            }
+//        }
+//        return 0;
+//    }
+//};
+
+
