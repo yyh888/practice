c57dab7199fa962f4177b0b7afdd6fd31fd1e39f
yyh888 <1161752748@qq.com>
2023-03-27 21:07:16 +0800

    刷题统计

---
+++ 蓝桥杯真题1/蓝桥杯真题1/practice.cpp
@@ -0,0 +1,40 @@
+// À¢Ã‚Õ≥º∆
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    long long a, b, n;
+//    cin >> a >> b >> n;
+//    long long sum = 0;
+//    long long cnt = 0;
+//    long long week = 5 * a + 2 * b;
+//    cnt = (n / week) * 7;
+//    n %= week;
+//    for (int i = 1; i <= 7; i++)
+//    {
+//        if (i <= 5)
+//        {
+//            if (sum >= n)
+//            {
+//                break;
+//            }
+//            sum += a;
+//            cnt++;
+//        }
+//        else
+//        {
+//            if (sum >= n)
+//            {
+//                break;
+//            }
+//            sum += b;
+//            cnt++;
+//        }
+//    }
+//    cout << cnt;
+//    return 0;
+//}
+
+
