ef3dec57b393acef4b16ef6eb49d49f927200a30
yyh888 <1161752748@qq.com>
2023-03-27 08:28:57 +0800

    数根

---
+++ 笔试训练25/笔试训练25/practice.cpp
@@ -0,0 +1,26 @@
+// Êý¸ù
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string s;
+//    while (cin >> s)
+//    {
+//        while (s.size() != 1)
+//        {
+//            int sum = 0;
+//            for (int i = 0; i < s.size(); i++)
+//            {
+//                sum += (s[i] - '0');
+//            }
+//            s = to_string(sum);
+//        }
+//        cout << s << endl;
+//    }
+//    return 0;
+//}
+
+
