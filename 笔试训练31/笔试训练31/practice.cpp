9445df79add7822d54702766bf41cb74c6143aec
yyh888 <1161752748@qq.com>
2023-04-02 20:42:50 +0800

    分解因数

---
+++ 笔试训练31/笔试训练31/practice.cpp
@@ -0,0 +1,35 @@
+// 分解因数
+//#include <iostream>
+//#include <cmath>
+//#include <string>
+//#include <vector>
+//#include <cstdio>
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    while (cin >> n)
+//    {
+//        printf("%d =", n);
+//        for (int i = 2; i <= sqrt(n); i++)
+//        {
+//            while (n != i)
+//            {
+//                if (n % i == 0)
+//                {
+//                    n /= i;
+//                    printf(" %d *", i);
+//                }
+//                else
+//                {
+//                    break;
+//                }
+//            }
+//        }
+//        printf(" %d\n", n);
+//    }
+//    return 0;
+//}
+
+
