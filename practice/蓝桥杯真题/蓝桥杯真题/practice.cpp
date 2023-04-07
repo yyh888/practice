27037ea91ed15c4a214de1a7e1490610d54f38cf
yyh888 <1161752748@qq.com>
2023-04-07 21:41:36 +0800

    蓝桥杯真题2

---
+++ practice/蓝桥杯真题/蓝桥杯真题/practice.cpp
@@ -0,0 +1,45 @@
+
+//#include <iostream>
+//#include <bitset>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	bitset<8> bt;
+//	int n, m;
+//	string tmp;
+//	while (cin >> n >> m)
+//	{
+//		bt = n;
+//		tmp = bt.to_string();
+//		for (auto& e : tmp)
+//		{
+//			if (e == '0')
+//			{
+//				e = ' ';
+//			}
+//			else
+//			{
+//				e = '*';
+//			}
+//		}
+//		cout << tmp;
+//		bt = m;
+//		tmp = bt.to_string();
+//		for (auto& e : tmp)
+//		{
+//			if (e == '0')
+//			{
+//				e = ' ';
+//			}
+//			else
+//			{
+//				e = '*';
+//			}
+//		}
+//		cout << tmp << endl;
+//	}
+//	return 0;
+//}
+
