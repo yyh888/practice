20be32647f998cc120d99fb4e8b8ed751a8cbbde
yyh888 <1161752748@qq.com>
2023-07-07 10:27:10 +0800

    抄送列表

---
+++ 笔试训练32/笔试训练32/practice.cpp
@@ -0,0 +1,49 @@
+// ³­ËÍÁĞ±í
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s, res;
+//	while (getline(cin, s))
+//	{
+//		getline(cin, res);
+//		bool sta = false;
+//		for (int i = 0; i < s.length(); i++)
+//		{
+//			string t;
+//			if (s[i] == '"')
+//			{
+//				i++;
+//				while (i < s.length() && s[i] != '"')
+//				{
+//					t += s[i];
+//					i++;
+//				}
+//				if (t == res)
+//				{
+//					sta = true;
+//					break;
+//				}
+//			}
+//			else if (s[i] != ',')
+//			{
+//				while (i < s.length() && s[i] != ',')
+//				{
+//					t += s[i];
+//					i++;
+//				}
+//				if (t == res)
+//				{
+//					sta = true;
+//					break;
+//				}
+//			}
+//		}
+//		if (sta == true)
+//			cout << "Ignore" << endl;
+//		else
+//			cout << "Important!" << endl;
+//	}
+//	return 0;
+//}
