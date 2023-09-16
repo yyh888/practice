75aa71c3300db3a2f09ce262b2c390a694c5e4a2
yyh888 <1161752748@qq.com>
2023-09-16 22:08:44 +0800

    复习

---
+++ 2023_9_16/2023_9_16/practice.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//bool cmp(vector<int>& n1, vector<int>& n2)
+//{
+//	return n1[0] < n2[0];
+//}
+//
+//int main()
+//{
+//	vector<vector<int>> nums{ {{70},{31,32, 33,34,35},{ 23,24 }} };
+//	sort(nums.begin(), nums.end(), cmp);
+//
+//	return 0;
+//}
+
+
