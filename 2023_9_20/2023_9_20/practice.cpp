7c5c74e0de7e110581b2b89d0e41925a29dffa89
yyh888 <1161752748@qq.com>
2023-09-20 19:14:42 +0800

    复习

---
+++ 2023_9_20/2023_9_20/practice.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    while (cin >> n)
+//    {
+//        if (n <= 3)
+//        {
+//            cout << n << endl;
+//            continue;
+//        }
+//        int a = 1, b = 2, c = 3;
+//        for (int i = 4; i <= n; i++)
+//        {
+//            int d = a + c;
+//            a = b;
+//            b = c;
+//            c = d;
+//        }
+//        cout << c << endl;
+//    }
+//    return 0;
+//}
+//// 64 Î»Êä³öÇëÓÃ printf("%lld")
+
+
+const int N = 10;
+
+
+int main()
+{
+	int a[N];
+
+	return 0;
+}
