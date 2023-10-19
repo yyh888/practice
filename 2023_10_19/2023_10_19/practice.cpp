bc91e35068f61c2781ff608ce874852b4fcbca34
yyh888 <1161752748@qq.com>
2023-10-19 21:46:19 +0800

    除自身以外数组的乘积

---
+++ 2023_10_19/2023_10_19/practice.cpp
@@ -0,0 +1,20 @@
+//class Solution {
+//public:
+//    vector<int> productExceptSelf(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<int> ans(n, 1);
+//        for (int i = 1; i < n; i++)
+//        {
+//            ans[i] = ans[i - 1] * nums[i - 1];
+//        }
+//        int tmp = 1;
+//        for (int i = n - 2; i >= 0; i--)
+//        {
+//            tmp *= nums[i + 1];
+//            ans[i] *= tmp;
+//        }
+//        return ans;
+//    }
+//};
+
+
