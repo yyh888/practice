c0a85e383fb66359c375f8ccaf87edc089a29689
yyh888 <1161752748@qq.com>
2023-11-01 19:16:37 +0800

    找出数组中的所有 K 近邻下标

---
+++ 2023_11_1/2023_11_1/practice.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> res;
+        int n = nums.size();
+        // 遍历数对
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (nums[j] == key && abs(i - j) <= k) {
+                    res.push_back(i);
+                    break;   // 提前结束以防止重复添加
+                }
+            }
+        }
+        return res;
+    }
+};
