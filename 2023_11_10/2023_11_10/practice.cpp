d08bfc0dd025fd69f85f076b05d2e3e8ab6d9fca
yyh888 <1161752748@qq.com>
2023-11-10 11:55:00 +0800

    咒语和药水的成功对数

---
+++ 2023_11_10/2023_11_10/practice.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (auto& i : spells) {
+            long long t = (success + i - 1) / i - 1;
+            res.push_back(potions.size() - (upper_bound(potions.begin(), potions.end(), t) - potions.begin()));
+        }
+        return res;
+    }
+};
