13ebbe04fa4a4136c2227b3ebae1f8d87d3de9a9
yyh888 <1161752748@qq.com>
2023-11-17 23:10:43 +0800

    将找到的值乘以 2

---
+++ 2023_11_17/2023_11_17/p.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(), nums.end());
+        for (int num : nums) {
+            if (original == num) {
+                original *= 2;
+            }
+        }
+        return original;
+    }
+};
+
